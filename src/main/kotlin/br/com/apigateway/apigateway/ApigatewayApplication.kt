fd600229a79df6067e52c51443b058e71b395d7f
Lucas Oliveira <56942616+lucasvieira-ol@users.noreply.github.com>
2023-04-24 21:00:33 -0300

    feature: initial structure

---
+++ src/main/kotlin/br/com/apigateway/apigateway/ApigatewayApplication.kt
@@ -0,0 +1,11 @@
+package br.com.apigateway.apigateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApigatewayApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApigatewayApplication>(*args)
+}
